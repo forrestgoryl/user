c1b91ef664a5238e416e47547de242437b1eed90
forrestgoryl <forrestgoryl@outlook.com>
2021-02-17 16:03:13 +0800

    chained methods

--- user.py
+++ user.py
@@ -6,25 +6,28 @@ class User:
     
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
     
     def display_user_balance(self):
         print(f"Here is {self.name}'s balance: {self.account_balance}.")
+        return self
 
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 
 Greg = User("Greg", "greg@gmail.com")
 Forrest = User("Forrest", "forrest@outlook.com")
 Serena = User("Serena", "serena@qq.com")
 
-print(Greg.account_balance, Forrest.account_balance, Serena.account_balance)
-Greg.make_deposit(50)
-Serena.make_deposit(150)
-Forrest.make_deposit(50)
-print(Greg.account_balance, Forrest.account_balance, Serena.account_balance)
-Serena.transfer_money(Forrest, 50)
-print(Greg.account_balance, Forrest.account_balance, Serena.account_balance)
+Greg.make_deposit(50).make_deposit(100).make_deposit(30).make_withdrawal(20)
+Forrest.make_deposit(50).make_deposit(200).transfer_money(Serena, 50)
+Serena.make_deposit(300).make_withdrawal(100)
+
+print(f"""Greg's balance is {Greg.account_balance}. Forrest's balance is {Forrest.account_balance}. 
+Serena's balance is {Serena.account_balance}.""")
